0f9063153eacaa4677438004631b3ab8e4714c7d
Kaviraj Jagadeesan <kavirajjdsn@gmail.com>
2025-10-21 22:49:10 +0530

    integers: implement INCR cmd

--- app/app.go
+++ app/app.go
@@ -54,6 +54,8 @@ func (app *App) handleConnection(conn net.Conn) {
 			app.handleSet(conn, cmd)
 		case "GET":
 			app.handleGet(conn, cmd)
+		case "INCR":
+			app.handleIncrement(conn, cmd)
 		case "RPUSH", "LPUSH":
 			app.handlePush(conn, cmd)
 		case "LRANGE":
@@ -124,7 +126,27 @@ func (app *App) handleGet(conn net.Conn, cmd *parser.Command) {
 		return
 	}
 
-	conn.Write(parser.EncodeBulkString(valStr))
+	// check if integer
+	valInt, err := strconv.Atoi(valStr)
+	if err == nil {
+		conn.Write([]byte(fmt.Sprintf(":%d\r\n", valInt)))
+	} else {
+		conn.Write(parser.EncodeBulkString(valStr))
+	}
+}
+
+func (app *App) handleIncrement(conn net.Conn, cmd *parser.Command) {
+	if len(cmd.Args) < 1 {
+		conn.Write(parser.EncodeWrongNumArgsError("incr"))
+		return
+	}
+
+	n, err := app.store.Increment(cmd.Args[0])
+	if err != nil {
+		conn.Write(parser.EncodeError(err))
+		return
+	}
+	conn.Write([]byte(fmt.Sprintf(":%d\r\n", n)))
 }
 
 func (app *App) handlePush(conn net.Conn, cmd *parser.Command) {
