ce46a68b1871f4704435782b27638adb3668813d
Kaviraj Jagadeesan <kavirajjdsn@gmail.com>
2025-10-22 19:33:52 +0530

    feat: support port binding to custom ports

--- app/main.go
+++ app/main.go
@@ -2,17 +2,34 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net"
 	"os"
+	"slices"
+)
+
+const (
+	portFlag    = "--port"
+	defaultPort = "6379"
 )
 
 func main() {
-	l, err := net.Listen("tcp", "0.0.0.0:6379")
+	port := defaultPort
+	args := os.Args
+	// check if port flag is given
+	portFlagIndex := slices.Index(args, portFlag)
+	if portFlagIndex != -1 {
+		if len(args) < portFlagIndex+2 {
+			log.Fatal("provide port number")
+		}
+		port = args[portFlagIndex+1]
+	}
+	l, err := net.Listen("tcp", ":"+port)
 	if err != nil {
-		fmt.Println("Failed to bind to port 6379")
+		fmt.Println("Failed to bind to port", port)
 		os.Exit(1)
 	}
-	fmt.Println("Listening on :6379")
+	fmt.Println("Listening on port", port)
 	app := newApp()
 	for {
 		conn, err := l.Accept()
